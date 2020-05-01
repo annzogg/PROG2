b96f619e4727bb8a433c98d6951b5111a3312684
annzogg <ann.zogg@hotmail.com>
2020-05-01 09:18:32 +0200

    HTML Seiten erstellt

--- start.py
+++ start.py
@@ -1,10 +1,36 @@
 from flask import Flask
+from flask import request
+from flask import render_template
 
-app = Flask("Hello World")
+app = Flask("Demo")
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello, World!'
+@app.route("/start/", methods=['GET', 'POST'])
+def start():
+	if request.method == 'POST':
+		ziel_person = request.form['vorname']
+		rueckgabe_string = "Hello " + ziel_person + "!"
+		return rueckgabe_string
+
+	return render_template("start.html")
+
+@app.route("/holiday/")
+def holiday(): 
+
+	return render_template("holiday.html")
+
+
+@app.route("/working/")
+def working():
+
+	return render_template("working.html")
+
+@app.route("/result/")
+def result(): 
+	unterlagen = ["Fahrausweis", "Visa/Passport/ID", "Impfausweis"]
+	return render_template("result.html", unterlagen = unterlagen)
+
+
+
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+	app.run(debug=True, port=5000)
