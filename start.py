4418115e9330d8c25c1074717611147815ec0fb8
annzogg <ann.zogg@hotmail.com>
2020-05-02 11:09:12 +0200

    Update Listen

--- start.py
+++ start.py
@@ -26,8 +26,25 @@ def working():
 
 @app.route("/result/")
 def result(): 
-	unterlagen = ["Fahrausweis", "Visa/Passport/ID", "Impfausweis"]
-	return render_template("result.html", unterlagen = unterlagen)
+	items = {
+	"unterlagen": ["Fahrausweis", "Visa/Passport/ID", "Impfausweis", "Flug-/Bahn-/Reiseticket", "Karte", "Reiseführer"],
+	"hygieneartikel": ["Zahnbürste & Zahnpaste", "Gesichtsreiniger", "Tagescreme", "Nachtcreme", "Wattestäbchen", "Pinzette", "Deodorant", "Shampoo und Duschgel", "Wäschesack", "Badetuch", "Nastücher"],
+	"elektronik": ["Handy & Ladegerät", "Kamera & Ladegerät", "Kopfhörer", "Adapter"],
+	"kleider": ["Unterhosen", "Socken", "Pyjama", "Gürtel", "Bikini/Badehose", "Regenjacke"],
+	"bformal": ["Anzug", "Hemd", "Anzugshose", "Anzugsschuhe", "Mantel", "Aktentasche", "Schmuck / Uhr", "Laptop & Ladegerät"],
+	"arbeit": ["Dokumente", "Schreibzeug", "Aktentasche", "Laptop & Ladegerät", "Kreditkarten", "Visitenkarten", "Notizpapier"],
+	"international": ["Visa", "Adapter", "Wörterbuch", "wichtigsten App herunterladen", "offline Karte auf Handy speichern"],
+	"wandern": ["Wanderschuhe", "Wanderhose", "Wander Shirt", "Wandersocken", "Jacke / Windstopper", "Mütze / Stirnband", "Trinkflasche", "Rucksack", "erste Hilfe Ausrüstung", "Taschenlampe"],
+	"joggen": ["Laufschuhe", "Laufhosen", "Lauf Shirt", "Jacke / Windstopper", "Trinkflasche", "Sporttuch"],
+	"biken": ["Bikehose / Bikeshirt", "Bikeschuhe", "Bike / Ort zum Mieten raussuchen", "Trinkflasche", "Bike Handschuhe", "Rucksack", "Sportbrille"],
+	"camping": ["Zelt", "Schlafmatte", "Feuerzeug", "Taschenlampe", "Trinkflasche", "Rucksack", "Trainer", "Jacke / Windstopper / Regenjacke", "Besteck / Teller", "Schlafsack"],
+	"strand": ["Badetuch", "Bikini / Badehose", "Sonnenbrille", "Strandtuch", "Sonnencreme", "Flipflops", "Tasche", "Buch / Spiel", "Lockere Kleidung"],
+	"nachtessen": ["Hemd", "Kleid", "Schöne Schuhe", "Schmuck", "Uhr", "Mantel"],
+	"fitness": ["Sporthose", "Sportshirt", "Sportschuhe", "Trinkflasche", "Sporttuch", "Sportsocken", "Kopfhörer"],
+	"foto": ["Kamera", "Speicherkarte", "Fototasche", "Ladegerät", "Kamera Linsen", "Putztzch für Kamera", "Stativ"],
+	}
+	
+	return render_template("result.html", items = items)
 
 
 
